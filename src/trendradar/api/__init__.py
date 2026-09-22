eaa656f3a1b8966baf31c1879643bd542414ff02
exasdwyh-commits <exasdwyh@gmail.com>
2026-09-22 17:44:45 +0800

    refactor: add API router package

---
+++ src/trendradar/api/__init__.py
@@ -0,0 +1,5 @@
+from .content import build_content_router
+from .editorial import build_editorial_router
+from .ops import build_ops_router
+
+__all__ = ["build_content_router","build_editorial_router","build_ops_router"]
